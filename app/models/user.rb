bdfac006f1c4c031ebed66f33376a02a1669b925
svella84 <svella84@gmail.com>
2014-02-08 17:12:30 +0100

    devise update2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_one :information, dependent: :destroy
+
+  accepts_nested_attributes_for :information, update_only: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :confirmable, :lockable
 end
