e50ae7e34f95f605cdad46adfc7e1cf798c4f192
svella84 <svella84@gmail.com>
2014-02-08 16:27:38 +0100

    devise update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Farmland::Application.initialize!
+
+Farmland::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  	address: "smtp.gmail.com",
+  	port: "587",
+  	domain: "baci.lindsaar.net",
+  	authentication: :plain,
+  	user_name: "farmland.sv@gmail.com",
+  	password: "free.willy",
+  	enable_starttls_auto: true
+  }
+end
